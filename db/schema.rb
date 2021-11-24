48357e5ff0447d7717b63a02f1b135ab5980c9b4
Abdelrahman Alshater <shater_11@hotmail.com>
2021-11-24 11:44:15 -0300

    Change the name of tour_guide? to tour_guide

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_191843) do
+ActiveRecord::Schema.define(version: 2021_11_24_143854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_191843) do
     t.string "first_name"
     t.string "last_name"
     t.string "country"
-    t.boolean "tour_guide?"
+    t.boolean "tour_guide"
     t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
