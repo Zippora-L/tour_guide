2680cd3c127b6758b2e23cd8013a735d69708b1e
Abdelrahman Alshater <shater_11@hotmail.com>
2021-11-25 22:24:35 -0300

    validations done

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,10 +3,15 @@ class Tour < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  validates :title, :description, :limit_of_people, :price, :duration, :destination, :photo, presence: { message: "must be given please" }
+  validates :title, :description, length: { minimum: 4 }
+  validates :description, length: { maximum: 500 }
+  validates :limit_of_people, :price, :duration, numericality: { only_integer: true }
+
   include PgSearch::Model
   pg_search_scope :search_by_title_and_description,
-    against: [ :title, :description ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+                  against: [ :title, :description ],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
